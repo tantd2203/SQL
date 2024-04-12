8e834a28b09fa2265b1c1d7f8ec62b099d8de50e
tantd9 <tranduytan.220300@gmail.com>
2024-04-12 21:03:52 +0700

    average selling price

---
+++ Basic Aggregate Functions/1251 Average Selling Price.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Prices (
+    product_id INT,
+    start_date DATE,
+    end_date DATE,
+    price DECIMAL(10, 2)
+);
+
+INSERT INTO Prices (product_id, start_date, end_date, price) VALUES
+(1, '2019-02-17', '2019-02-28', 5),
+(1, '2019-03-01', '2019-03-22', 20),
+(2, '2019-02-01', '2019-02-20', 15),
+(2, '2019-02-21', '2019-03-31', 30);
+
+CREATE TABLE UnitsSold (
+    product_id INT,
+    purchase_date DATE,
+    units INT
+);
+
+INSERT INTO UnitsSold (product_id, purchase_date, units) VALUES
+(1, '2019-02-25', 100),
+(1, '2019-03-01', 15),
+(2, '2019-02-10', 200),
+(2, '2019-03-22', 30);
+
+-- Result 
+# Write your MySQL query statement below
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND
+u.purchase_date BETWEEN start_date AND end_date
+group by product_id
